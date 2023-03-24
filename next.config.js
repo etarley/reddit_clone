4653ba57e27a32fe818200bedfe24b6aa56f6e2b
Etarley <standecristo@gmail.com>
2023-03-23 23:55:55 -0400

    Add modularize imports to Nextjs

--- next.config.js
+++ next.config.js
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   experimental: {
     appDir: true,
   },
-}
+  modularizeImports: {
+    '@chakra-ui/react': {
+      transform: '@chakra-ui/react/{{member}}',
+    },
+    '@chakra-ui/icons': {
+      transform: '@chakra-ui/icons/{{member}}',
+    },
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
